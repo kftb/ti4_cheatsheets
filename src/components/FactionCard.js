ab86498253371186ba7b3e6468b2312cb42963ad
Kevin Bay <kevin.bay@outlook.com>
2022-08-08 18:34:16 -0700

    Update styling on FactionCards

--- src/components/FactionCard.js
+++ src/components/FactionCard.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
 
-function FactionCard({ faction }) {
-  const {
-    register,
-    formState: { errors },
-  } = useFormContext();
+function FactionCard({ faction, factionCounter }) {
+  const { register } = useFormContext();
 
   const { factionName, subtitle, id } = faction;
   return (
     <div>
       <li>
         <input
-          {...register(`${id}`)}
+          {...register(`factions.${id}`)}
           type="checkbox"
           id={`${id}-option`}
           value=""
@@ -21,12 +18,24 @@ function FactionCard({ faction }) {
         />
         <label
           htmlFor={`${id}-option`}
-          className="inline-flex justify-between items-center p-5 w-full text-gray-500 bg-white rounded-lg border-2 border-gray-200 cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-blue-600 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700"
+          className="inline-flex justify-between items-center px-3 py-2 w-full 
+            rounded-lg border-2  cursor-pointer 
+            text-gray-400 bg-gray-800 border-gray-700  grayscale
+            hover:bg-gray-700 hover:text-gray-200 hover:grayscale-0
+             peer-checked:border-blue-600 peer-checked:text-gray-300 peer-checked:grayscale-0
+             transition ease-in-out duration-400
+            "
         >
-          <div className="block">
-            <div className="w-full text-lg font-semibold">{factionName}</div>
-            <div className="w-full text-sm">{subtitle}</div>
+          <div className="flex flex-1 items-center justify-between ">
+            <div>
+              <div className="w-full text-lg font-semibold">{factionName}</div>
+              <div className="w-full text-sm">{subtitle}</div>
+            </div>
           </div>
+          <img
+            className="object-cover ml-4 rounded-full h-20"
+            src={`/factions/portraits/${id}.png`}
+          />
         </label>
       </li>
     </div>
