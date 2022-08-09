6b852e86a5288989d04fe2da9ecfe1ed4b57655a
Kevin Bay <kevin.bay@outlook.com>
2022-08-08 22:47:24 -0700

    Expand config and set default

--- src/context/SetupContext.js
+++ src/context/SetupContext.js
@@ -2,9 +2,9 @@ import uuid from 'react-uuid';
 import create from 'zustand';
 
 export const useSetupStore = create((set) => ({
-  factions: ['arborec', 'argent', 'barony', 'jolnar', 'l1z1x', 'titans', 'xxcha', 'yssaril'],
+  factions: [],
 
-  config: { page_size: 'letter' },
+  config: { page_size: 'a4' },
 
   addFaction: (faction) => set((state) => ({ factions: faction })),
   addConfig: (config) => set((state) => ({ config: config })),
