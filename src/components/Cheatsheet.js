6a298e2f13e2b31faa5b8c4787e42cfdcebbbd80
Kevin Bay <kevin.bay@outlook.com>
2022-08-08 18:34:36 -0700

    Update cheatsheet generation and PDF Viewer

--- src/components/Cheatsheet.js
+++ src/components/Cheatsheet.js
@@ -1,53 +1,62 @@
 import React from 'react';
 import { Page, Text, View, Document, StyleSheet, PDFViewer, Image } from '@react-pdf/renderer';
 import { uniqueId } from 'lodash';
+import { useSetupStore } from '../context/SetupContext';
+import { getEveryNth } from '../libs/getEveryNth';
 
-// Create styles
-const styles = StyleSheet.create({
-  page: {
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-  },
-  section: {
-    flex: 1,
+function Cheatsheet() {
+  // load selected factions loaded in from previous page
+  // TODO: Consider parsing them into the url instead
+  const selectedFactions = useSetupStore((state) => state.factions);
 
-    alignContent: 'center',
-  },
-  image: {
-    marginVertical: 2,
-    marginHorizontal: 5,
-    maxWidth: '95%',
-  },
-});
+  // Create styles
+  const styles = StyleSheet.create({
+    page: {
+      flexDirection: 'row',
+      backgroundColor: '#fff',
+    },
+    section: {
+      flex: 1,
 
-const factions = ['arborec', 'nomad', 'winnu', 'argentflight', 'empyrean', 'federationofsol'];
+      alignContent: 'center',
+    },
+    image: {
+      marginVertical: 2,
+      marginHorizontal: 5,
+      maxWidth: '95%',
+    },
+  });
 
-const factionsLeft = factions.slice(0, 4);
-const factionsRight = factions.slice(4);
+  const factions = selectedFactions;
+  const factionsLeft = factions.slice(0, 4);
+  const factionsRight = factions.slice(4);
 
-const factionViewLeftImages = factionsLeft.map((fl) => (
-  <Image key={uniqueId()} style={styles.image} src={`/factions/${fl}.png`} />
-));
+  const factionViewLeftImages = factionsLeft.map((fl) => (
+    <Image key={uniqueId()} style={styles.image} src={`/factions/cards/${fl}.png`} />
+  ));
 
-const factionViewRightImages = factionsRight.map((fl) => (
-  <Image key={uniqueId()} style={styles.image} src={`/factions/${fl}.png`} />
-));
+  const factionViewRightImages = factionsRight.map((fr) => (
+    <Image key={uniqueId()} style={styles.image} src={`/factions/cards/${fr}.png`} />
+  ));
 
-const pdf = (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>{factionViewLeftImages}</View>
-      <View style={styles.section}>{factionViewRightImages}</View>
-    </Page>
-  </Document>
-);
+  const pdf = (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>{factionViewLeftImages}</View>
+        <View style={styles.section}>{factionViewRightImages}</View>
+      </Page>
+    </Document>
+  );
 
-function CheatSheet() {
   return (
-    <PDFViewer width="1000" height="1500">
-      {pdf}
-    </PDFViewer>
+    <>
+      <div className="w-full">
+        <PDFViewer width="100%" height="1500px">
+          {pdf}
+        </PDFViewer>
+      </div>
+    </>
   );
 }
 
-export default CheatSheet;
+export default Cheatsheet;
