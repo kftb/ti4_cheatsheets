34f9b3d553aea5927ab8ac1fd0c1415d67f943ee
Kevin Bay <kevin.bay@outlook.com>
2022-08-08 22:29:10 -0700

    Expand context with config

--- src/context/SetupContext.js
+++ src/context/SetupContext.js
@@ -2,7 +2,10 @@ import uuid from 'react-uuid';
 import create from 'zustand';
 
 export const useSetupStore = create((set) => ({
-  factions: ['arborec'],
+  factions: ['arborec', 'argent', 'barony', 'jolnar', 'l1z1x', 'titans', 'xxcha', 'yssaril'],
+
+  config: { page_size: 'letter' },
 
   addFaction: (faction) => set((state) => ({ factions: faction })),
+  addConfig: (config) => set((state) => ({ config: config })),
 }));
