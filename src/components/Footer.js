6290dc1a6b16ac5cfa2bc6a85491c7916499b220
Kevin Bay <kevin.bay@outlook.com>
2023-04-09 11:05:16 -0700

    chore: Update version number

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function HomepageFooter() {
   return (
     <div className="my-10 text-gray-500 text-sm">
-      Created with ❤️ and 🦁 by <a href="https://github.com/kftb">kftb</a> | v1.2.1
+      Created with ❤️ and 🦁 by <a href="https://github.com/kftb">kftb</a> | v1.3.0
     </div>
   );
 }
