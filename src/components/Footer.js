ccee17fadd8e40f9f6991ec4e1ac2d8cd995634a
Kevin Bay <kevin.bay@outlook.com>
2026-03-30 22:41:59 -0700

    chore: Bump version to 1.5.0

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function HomepageFooter() {
   return (
     <div className="my-10 text-gray-500 text-sm">
-      Created with ❤️ and 🦁 by <a href="https://github.com/kftb">kftb</a> | v1.4.0
+      Created with ❤️ and 🦁 by <a href="https://github.com/kftb">kftb</a> | v1.5.0
     </div>
   );
 }
